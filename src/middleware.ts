e5a02abcf377e636538f8bd0107b0dd3dd4b6406
te ara <arat252539@gmail.com>
2025-08-14 12:18:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,6 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
+  // Make sure this is configured correctly for your app
   matcher: ['/', '/(ja|en)/:path*']
 };
