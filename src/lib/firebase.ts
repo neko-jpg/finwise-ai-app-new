d8662f1305940b0d10d94d944f8c61daf02749c5
te ara <arat252539@gmail.com>
2025-08-14 08:48:11 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  "projectId": "finwise-ai-s18cs",
+  "appId": "1:1016695664403:web:798a406d41c0f52a5734e6",
+  "storageBucket": "finwise-ai-s18cs.firebasestorage.app",
+  "apiKey": "AIzaSyB6_ckmGVee3vM-LgykEVXnrJbAOW2PeTU",
+  "authDomain": "finwise-ai-s18cs.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1016695664403"
+};
+
+// Initialize Firebase
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { firebaseApp };
