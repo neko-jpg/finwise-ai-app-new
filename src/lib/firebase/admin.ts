27e6e41dfdf65d97927e95f4bedd93c2db3bef3d
neko-jpg <arat252539@gmail.com>
2025-08-17 17:58:51 +0900

    Update admin.ts

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,15 +1,12 @@
-import { cert, getApps, initializeApp, getApp, App } from 'firebase-admin/app';
-import { getFirestore, Firestore } from 'firebase-admin/firestore';
+// src/lib/firebase/admin.ts
+import { getApps, initializeApp, cert } from 'firebase-admin/app';
 
-const app: App = getApps().length
-  ? getApp()
-  : initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
-      }),
-    });
-
-const adminDb: Firestore = getFirestore(app);
-export { app as adminApp, adminDb };
+export const adminApp =
+  getApps()[0] ??
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    }),
+  });
